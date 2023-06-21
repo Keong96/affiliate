b777a3188c3df3df85e91501a5ed54a47beabf11
Keong96 <chinkeongtan96@gmail.com>
2023-06-21 19:42:44 +0800

    init

--- index.js
+++ index.js
@@ -27,3 +27,84 @@ app.listen(PORT, () => {
 app.get('/', async (req, res) => {
   res.status(200).send("OK");
 })
+
+app.post('/user/login', async (req, res) => {
+
+  if( typeof(req.body.email) == 'undefined' || typeof(req.body.password) == 'undefined')
+  {
+    return res.status(500).send("Error: Please enter your email and password to login.");
+  }
+
+  client.query("SELECT * FROM users WHERE email = '"+req.body.email+"' AND password = crypt('"+req.body.password+"', password)")
+        .then((result) => {
+          if(result.rows.length > 0)
+          {
+            const token = GenerateJWT(result.rows[0].id, result.rows[0].email, result.rows[0].username);
+
+            client.query("UPDATE users SET last_login = NOW() WHERE id = "+result.rows[0].id)
+
+            res.status(200).json({
+                success: true,
+                data: {
+                  userId: result.rows[0].id,
+                  email: result.rows[0].email,
+                  token: token,
+                },
+              });
+          }
+          else
+          {
+            res.status(500).send("Error: Wrong Username or Password");
+          }
+        })
+        .catch((e) => {
+          console.error(e.stack);
+          res.status(500).send(e.stack);
+        })
+})
+
+app.post('/user/create/:referral?', async (req, res) => {
+
+  if( typeof(req.body.email) == 'undefined' || typeof(req.body.password) == 'undefined' || typeof(req.body.username) == 'undefined')
+  {
+    return res.status(500).send("Error: Please fill in your username, email, and password to complete the registration process.");
+  }
+
+  client.query("SELECT * FROM users WHERE email = '"+req.body.email+"'")
+        .then((result) => {
+            if(result.rows.length > 0)
+            {
+              if(req.body.email == result.rows[0].email)
+                return res.status(500).send("Error: Email has been taken");
+            }
+            else
+            {
+              client.query("INSERT INTO users (username, email, password) VALUES ('"+req.body.username+"', '"+req.body.email+"', crypt('"+req.body.password+"', gen_salt('bf')))")
+                    .then((result) => {
+                      if (req.params.referral)
+                      {
+                        client.query("INSERT INTO referral (user_id, username, referral) VALUES ('"+result.insertId+"', '"+req.body.username+"', '"+req.params.referral+"')");
+                      }
+
+                      res.status(201).send("Register Success");
+                    })
+                    .catch((e) => {
+                      console.error(e.stack);
+                      res.status(500).send(e.stack);
+                    })
+            }
+        })
+        .catch((e) => {
+          console.error(e.stack);
+          res.status(500).send(e.stack);
+        })
+})
+
+app.get('/user/get/:id', async (req, res) => {
+      client.query("SELECT * FROM users WHERE id = "+req.params.id)
+            .then((result) => res.send(JSON.stringify(result.rows[0])))
+            .catch((e) => {
+                console.error(e.stack);
+                res.status(500).send(e.stack);
+            })
+})
